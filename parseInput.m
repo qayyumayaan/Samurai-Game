5efb561880023540ded1a33c47b9e83004c0fb56
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2022-09-26 09:05:46 -0400

    Update

--- parseInput.m
+++ parseInput.m
@@ -69,19 +69,24 @@ function [battleground,playerX,playerY,userInput,enemyIndex,enemyNumber] = parse
             end
 
         case "attack2"
-            attackPower = 50;
-            for iY = 1:1
-                for iX = -X:X
+
+            for iY = -1:1
+                for iX = -1:1
                     if playerY+iY > Y || playerY+iY <= 0 || playerX+iX > X  || playerX+iX <= 0
-                        
                     else
                         if battleground(playerY+iY,playerX+iX) < 0
-                            if battleground(playerY+iY,playerX+iX) + attackPower > 0
+                            if battleground(playerY+iY,playerX+iX) + attackPower >= 0
                                 battleground(playerY+iY,playerX+iX) = 0;
-                                disp(" ");
-                                disp("Enemy eliminated!");
+                            fprintf("\n Enemy eliminated!");
                             else
-                                battleground(playerY+iY,playerX+iX) = battleground(playerY+iY,playerX+iX) + attackPower; 
+                                for iEnemy = 1:enemyNumber
+                                    if enemyIndex(1,iEnemy) == battleground(playerY+iY,playerX+iX)
+                                        battleground(playerY+iY,playerX+iX) = battleground(playerY+iY,playerX+iX) + attackPower; 
+                                        enemyIndex(1,iEnemy) = battleground(playerY+iY,playerX+iX);
+                                        disp("15 damage dealt!");  
+                                    end
+                                end
+
                             end
                         end
                     end
